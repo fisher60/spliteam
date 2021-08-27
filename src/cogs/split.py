269a248ace6950cf6cf31080e7a63653db586957
GDWR <gregory.dwr@gmail.com>
2021-08-27 19:59:36 +0100

    Add dataclass that represents the json file.

--- src/cogs/split.py
+++ src/cogs/split.py
@@ -1,17 +1,55 @@
 import json
+from dataclasses import dataclass
 
-from aiofiles import open as aopen
 from discord import VoiceChannel
-from discord.ext.commands import Bot, Context, Cog, group
-
+from discord.ext.commands import Bot, Context, Cog, group, command
 
 from ..settings import SAVE_DATA_FILE
 
 
+@dataclass
+class SplitConfig:
+    """
+    Config is read from SAVE_DATA_FILE when this class in instantiated.
+
+    When a value is updated, this will be written to the file.
+    """
+    lobby_channel: int
+
+    def __init__(self):
+        self.__ensure_file_exits()
+        self.__sync_with_file()
+
+    @staticmethod
+    def __ensure_file_exits() -> None:
+        """If the file is not created, create it."""
+        if not SAVE_DATA_FILE.exists():
+            SAVE_DATA_FILE.write_text("{}")
+
+    def __sync_with_file(self):
+        """Sync this dataclass with the file content"""
+        with open(SAVE_DATA_FILE) as f:
+            data = json.loads(f.read())
+
+        for key in data:
+            setattr(self, key, data[key])
+
+    def __write_file(self) -> None:
+        """Write the current dataclass to SAVE_DATA_FILE"""
+        with open(SAVE_DATA_FILE, "w") as f:
+            json.dump(vars(self), f)
+
+    def __setattr__(self, key: str, value) -> None:
+        self.__dict__[key] = value
+        self.__write_file()
+
+
 class Split(Cog):
     """Utilities for splitting up teams."""
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.data = SplitConfig()
 
     @group()
     async def config(self, ctx: Context):
