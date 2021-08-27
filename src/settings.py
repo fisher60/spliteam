8761bec5ab505fa1981f35854018a7aaa02906c2
GDWR <gregory.dwr@gmail.com>
2021-08-27 19:59:19 +0100

    Update path for `save_data.json`

--- src/settings.py
+++ src/settings.py
@@ -6,4 +6,4 @@ COMMAND_PREFIX = setting("COMMAND_PREFIX", default=".")
 
 TOKEN = setting("BOT_TOKEN", required=True)
 
-SAVE_DATA_FILE = setting("SAVE_DATA_FILE", default=Path.cwd() / "src" / "save_data.json", _type=Path)
+SAVE_DATA_FILE = setting("SAVE_DATA_FILE", default=Path.cwd() / "save_data.json", _type=Path)
